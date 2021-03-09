4353d08fa6b86add59fbedf5b34ab2fdfaab72a2
Jose Manuel <31418107+hosce@users.noreply.github.com>
2021-03-09 12:57:37 -0600

    Correccion de paso 2 solicitud
    Correccion de paso 2 solicitud

--- assets/BD/sare.sql
+++ assets/BD/sare.sql
@@ -13,12 +13,10 @@
 
 
 -- Volcando estructura de base de datos para sare
-DROP DATABASE IF EXISTS `sare`;
 CREATE DATABASE IF NOT EXISTS `sare` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci */;
 USE `sare`;
 
 -- Volcando estructura para tabla sare.dg_establecimiento
-DROP TABLE IF EXISTS `dg_establecimiento`;
 CREATE TABLE IF NOT EXISTS `dg_establecimiento` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_comercial` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
@@ -44,12 +42,14 @@ CREATE TABLE IF NOT EXISTS `dg_establecimiento` (
   `pesonal_ocupado` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `servicios_existentes` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.dg_establecimiento: ~0 rows (aproximadamente)
+DELETE FROM `dg_establecimiento`;
+/*!40000 ALTER TABLE `dg_establecimiento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dg_establecimiento` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sare.dimensiones_establecimiento
-DROP TABLE IF EXISTS `dimensiones_establecimiento`;
 CREATE TABLE IF NOT EXISTS `dimensiones_establecimiento` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `frente` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
@@ -62,10 +62,28 @@ CREATE TABLE IF NOT EXISTS `dimensiones_establecimiento` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.dimensiones_establecimiento: ~0 rows (aproximadamente)
+DELETE FROM `dimensiones_establecimiento`;
+/*!40000 ALTER TABLE `dimensiones_establecimiento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dimensiones_establecimiento` ENABLE KEYS */;
+
+-- Volcando estructura para tabla sare.director
+CREATE TABLE IF NOT EXISTS `director` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_expediente` int(11) NOT NULL DEFAULT '0',
+  `status` int(2) NOT NULL DEFAULT '2' COMMENT '0->rechazado 1-> aprobado 2->pendiente',
+  `recibido` datetime NOT NULL,
+  `visto` datetime NOT NULL,
+  `resuelto` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- Volcando datos para la tabla sare.director: ~0 rows (aproximadamente)
+DELETE FROM `director`;
+/*!40000 ALTER TABLE `director` DISABLE KEYS */;
+/*!40000 ALTER TABLE `director` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sare.expedientes
-DROP TABLE IF EXISTS `expedientes`;
 CREATE TABLE IF NOT EXISTS `expedientes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `folio` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
@@ -78,10 +96,12 @@ CREATE TABLE IF NOT EXISTS `expedientes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.expedientes: ~0 rows (aproximadamente)
+DELETE FROM `expedientes`;
+/*!40000 ALTER TABLE `expedientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expedientes` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sare.personas_fisicas
-DROP TABLE IF EXISTS `personas_fisicas`;
 CREATE TABLE IF NOT EXISTS `personas_fisicas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_completo` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
@@ -94,15 +114,21 @@ CREATE TABLE IF NOT EXISTS `personas_fisicas` (
   `c_p` int(10) NOT NULL,
   `rfc` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
   `curp` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `telefono` int(15) DEFAULT NULL,
+  `telefono` varchar(15) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `email` varchar(150) COLLATE utf8_spanish2_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.personas_fisicas: ~2 rows (aproximadamente)
+DELETE FROM `personas_fisicas`;
+/*!40000 ALTER TABLE `personas_fisicas` DISABLE KEYS */;
+INSERT INTO `personas_fisicas` (`id`, `nombre_completo`, `calle`, `no_exterior`, `no_interior`, `colonia`, `municipio`, `localidad`, `c_p`, `rfc`, `curp`, `telefono`, `email`) VALUES
+	(1, 'jose manuel castañeda espino', 'la misma', '123', '', 'la colonia', 'Jesus Maria', 'la misma', 20292, 'CAEM860323ES3', 'CAEM860323HASSSS00', '2147483647', 'correo@gmail.com'),
+	(2, 'jose manuel', 'la misma calle', '258', '', 'la otra colonia', 'Jesus Maria', 'cualquiera', 20920, 'CAEM860323ES3', 'CAEM860323HASSSN00', '1234567890', 'correo@email.com'),
+	(3, 'Jose Manuel Castañeda Espino', 'Casa Blanca', '806', '', 'casa blanca', 'Aguascalientes', 'jesus maria', 20900, 'CAEM860323ES3', 'CAEM860323HASSSN00', '4496330125', 'jose.espino@jesusmaria.gob.mx');
+/*!40000 ALTER TABLE `personas_fisicas` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sare.personas_morales
-DROP TABLE IF EXISTS `personas_morales`;
 CREATE TABLE IF NOT EXISTS `personas_morales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_empresa` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
@@ -126,21 +152,77 @@ CREATE TABLE IF NOT EXISTS `personas_morales` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.personas_morales: ~0 rows (aproximadamente)
+DELETE FROM `personas_morales`;
+/*!40000 ALTER TABLE `personas_morales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personas_morales` ENABLE KEYS */;
+
+-- Volcando estructura para tabla sare.secretario
+CREATE TABLE IF NOT EXISTS `secretario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_expediente` int(11) NOT NULL DEFAULT '0',
+  `status` int(2) NOT NULL DEFAULT '2' COMMENT '0->rechazado 1->aprobado 2->rechazado',
+  `recibido` datetime NOT NULL,
+  `visto` datetime NOT NULL,
+  `reselto` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- Volcando datos para la tabla sare.secretario: ~0 rows (aproximadamente)
+DELETE FROM `secretario`;
+/*!40000 ALTER TABLE `secretario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `secretario` ENABLE KEYS */;
+
+-- Volcando estructura para tabla sare.suelo
+CREATE TABLE IF NOT EXISTS `suelo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_expediente` int(11) NOT NULL DEFAULT '0',
+  `status` int(2) NOT NULL DEFAULT '2' COMMENT '0->rechazado 1->aprobado 2->Pendiente',
+  `recibido` datetime NOT NULL,
+  `visto` datetime NOT NULL,
+  `resuelto` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='uso';
+
+-- Volcando datos para la tabla sare.suelo: ~0 rows (aproximadamente)
+DELETE FROM `suelo`;
+/*!40000 ALTER TABLE `suelo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `suelo` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sare.usuarios
-DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_usuario` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `usuario` varchar(25) COLLATE utf8_spanish2_ci NOT NULL,
   `password` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `id_tipo_usuario` int(11) NOT NULL,
-  `status` tinyint(4) DEFAULT NULL,
+  `status` tinyblob NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sare.usuarios: ~2 rows (aproximadamente)
+DELETE FROM `usuarios`;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `usuario`, `password`, `id_tipo_usuario`, `status`) VALUES
+	(1, 'SUPER ADMIN', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, _binary 0x31),
+	(2, 'Fernando', 'fermar', 'e10adc3949ba59abbe56e057f20f883e', 1, _binary 0x31);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+-- Volcando estructura para tabla sare.ventanilla
+CREATE TABLE IF NOT EXISTS `ventanilla` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_expediente` int(11) NOT NULL DEFAULT '0',
+  `status` int(2) NOT NULL DEFAULT '2' COMMENT '0 -> rechazado 1 -> aprobado 2-> Pendiente',
+  `recibido` datetime NOT NULL,
+  `visto` datetime NOT NULL,
+  `resuelto` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- Volcando datos para la tabla sare.ventanilla: ~0 rows (aproximadamente)
+DELETE FROM `ventanilla`;
+/*!40000 ALTER TABLE `ventanilla` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventanilla` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
