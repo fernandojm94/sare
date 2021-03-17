dcc60fc4de0008e46acf0a1ca44ca5d400a185f4
fernandojm94 <69207654+fernandojm94@users.noreply.github.com>
2021-03-17 13:57:15 -0600

    Agrega funcionabilidad a anexos
    Agrega los "TABS" para ordena las solicitudes atendidas por las dependencias externas por "pendientes" y "atendidas".
    
    Envía los archivos de "anexos" al modelo.

---
+++ view/anexos/tabs.php
@@ -0,0 +1,104 @@
+<?php
+	$id_tab = $_POST['id'];
+
+	// $tipo = $_POST['algo']; SIRVE PARA IDENTIFICAR DESDE DONDE LLAMAN AL MODAL, DESDE DIRECTOR O DESDE SECRETARIO
+	$tipo = 2; //ASIGNACION PARA QUE NO MARQUE ERROR
+	$ausencia = 0;
+?>
+
+<div id="<?=$id_tab;?>" class="tab-pane in active">
+	<div class="message-container">
+		<div id="id-message-list-navbar" class="message-navbar clearfix">
+			<div class="pull-right tableTools-container"></div>
+		</div>
+
+		<div class="message-list-container">
+			<div class="message-list" id="message-list">
+				<div>
+					<table id="dynamic-table" class="table table-striped table-bordered table-hover">
+						<thead>
+							<tr>
+								<th>
+									<i class="ace-icon fa fa-clock-o bigger-110 ico_hid"></i>
+									Fecha y hora de creación
+								</th>
+
+								<th>
+									<i class="ace-icon fa fa-building bigger-110 ico_hid"></i>
+									Nombre establecimiento
+								</th>
+
+								<th class="hid_xs">
+									<i class="ace-icon fa fa-map-marker bigger-110 ico_hid"></i>
+									Dirección
+								</th>
+
+								<th class="hidden"></th>
+								<th class="hidden"></th>
+
+
+								<th class="hid_xs">
+									<i class="ace-icon fa fa-phone bigger-110 ico_hid"></i>
+									Número de teléfono
+								</th>
+
+								<th>
+									<i class="ace-icon fa fa-cogs bigger-110 ico_hid"></i>
+									Acciones
+								</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<tr>
+								<td>10 de noviembre 2020 15:34:21</td>
+								<td>Abarrotes Mi casita</td>
+								<td class="hid_xs">
+									Emiliano Zapata 109 Centro, Jesús María
+								</td>
+								<td class="hidden"></td>
+								<td class="hidden"></td>
+								<td class="hid_xs">449 121 1213</td>
+								<td class="center">
+									<div class="btn-group">
+										<a class="btn btn-xs btn-info" onclick="fill_modal_info(1,<?= $tipo; ?>,<?= $ausencia; ?>)" role="button" data-toggle="modal">
+											<i class="ace-icon fa fa-info-circle bigger-130"></i>
+										</a>
+
+										<a class="btn btn-xs btn-primary" onclick="fill_modal_update_anexos(1)" role="button" data-toggle="modal">
+											<i class="ace-icon fa fa-upload bigger-130"></i>
+										</a>
+									</div>
+								</td>
+							</tr>
+
+							<!-- <tr>
+								<td>2 de diciembre 2020 12:04:55</td>
+								<td>Cadena Comercial Oxxo</td>
+								<td class="hid_xs">
+									Av. López Mateos 1024, Lomas de Jesús María
+								</td>
+								<td class="hidden"></td>
+								<td class="hid_xs">449 895 7852</td>
+								<td class="center"><span class="label label-danger arrowed-right">2. Comprobante de pago.</span></td>
+								<td class="center">
+									<div class="btn-group">
+										<a class="btn btn-xs btn-info" onclick="fill_modal_info(2)" role="button" data-toggle="modal">
+											<i class="ace-icon fa fa-info-circle bigger-130"></i>
+										</a>
+
+										<a class="btn btn-xs btn-success" onclick="fill_modal_upcomprobante(2)" role="button" data-toggle="modal">
+											<i class="ace-icon fa fa-upload bigger-130"></i>
+										</a>
+									</div>
+								</td>
+							</tr> -->
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
