e0de46f98a00efc293079257bf119165c30b30a8
fernandojm94 <69207654+fernandojm94@users.noreply.github.com>
2021-03-31 14:26:23 -0600

    Fix texto de input
    En base al tipo de input; agrega el texto correspondiente.

--- view/solicitud/nuevo.php
+++ view/solicitud/nuevo.php
@@ -151,7 +151,7 @@
 
 
                                     <div class="form-group">
-                                        <label class="col-md-4 control-label">Horario de trabajo</label>  
+                                        <label class="col-md-4 control-label">Fecha de construcción</label>  
                                         <div class="col-md-4 inputGroupContainer">
                                             <div class="input-group">
                                                 <span class="input-group-addon"><i class="fa fa-building"></i></span>
