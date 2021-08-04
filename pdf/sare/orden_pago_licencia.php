8c7640ad0cf6460042cada9e7311c13f56001d66
fernandojm94 <emmanuel.martinez@jesusmaria.gob.mx>
2021-08-04 11:47:51 -0500

    Update orden_pago_licencia.php
    Agrega el formato para todos los pdf's
    Agrega variables globales ya que no entraban a las clases del TCPDF y por esa razón no se imprimían en el pdf.

--- pdf/sare/orden_pago_licencia.php
+++ pdf/sare/orden_pago_licencia.php
@@ -1,6 +1,5 @@
 <?php
     include('../../model/solicitud/fill.php');
-    
     $id_expediente = $_GET['id'];
     $expediente = fill_expediente($id_expediente);
     $establecimiento = fill_establecimiento($expediente['id_dg_establecimiento']);
@@ -13,184 +12,158 @@
     }
         
     // $nombre_persona = fill_nombre_persona($expediente['id_persona']);
+    $GLOBALS['monto'] = $_GET['monto'];
+    $GLOBALS['folio'] = $expediente['folio'];
+    $GLOBALS['nombre'] = $persona['nombre']; 
+    $GLOBALS['giro'] = $establecimiento['giro_scian'];
 
-    /**
-     * Clase que implementa un coversor de números
-     * a letras.
-     *
-     * Soporte para PHP >= 5.4
-     * Para soportar PHP 5.3, declare los arreglos
-     * con la función array.
-     *
-     * @author AxiaCore S.A.S
-     *
-     */
-    
-    $monto = $_GET['monto'];
-    $folio = $expediente['folio'];
-    $nombre = $persona['nombre']; 
-    $giro = $establecimiento['giro_scian'];
-
-    $letras = NumeroALetras::convertir($monto, 'pesos', 'centavos');
-    $letras = strtolower($letras);
-    $letras = ucfirst($letras);
+    $GLOBALS['letras'] = NumeroALetras::convertir($GLOBALS['monto'], 'pesos', 'centavos');
+    $GLOBALS['letras'] = strtolower($letras);
+    $GLOBALS['letras'] = ucfirst($letras);
 
     $today = date("d.m.Y");
     $today = str_replace('.', ' / ', $today);
 
-    /*
-        FALTA RECIBIR: 
-
-            -DESTINATARIO
-            -MONTO EN NÚMERO Y LETRA
-            -CONCEPTO DEL TRÁMITE
-            -MANDAR EL USUARIO QUE EMITE LA ORDEN
-    */
-
-
     class NumeroALetras
-{
-    private static $UNIDADES = [
-        '',
-        'UN ',
-        'DOS ',
-        'TRES ',
-        'CUATRO ',
-        'CINCO ',
-        'SEIS ',
-        'SIETE ',
-        'OCHO ',
-        'NUEVE ',
-        'DIEZ ',
-        'ONCE ',
-        'DOCE ',
-        'TRECE ',
-        'CATORCE ',
-        'QUINCE ',
-        'DIECISEIS ',
-        'DIECISIETE ',
-        'DIECIOCHO ',
-        'DIECINUEVE ',
-        'VEINTE '
-    ];
-
-    private static $DECENAS = [
-        'VENTI',
-        'TREINTA ',
-        'CUARENTA ',
-        'CINCUENTA ',
-        'SESENTA ',
-        'SETENTA ',
-        'OCHENTA ',
-        'NOVENTA ',
-        'CIEN '
-    ];
-
-    private static $CENTENAS = [
-        'CIENTO ',
-        'DOSCIENTOS ',
-        'TRESCIENTOS ',
-        'CUATROCIENTOS ',
-        'QUINIENTOS ',
-        'SEISCIENTOS ',
-        'SETECIENTOS ',
-        'OCHOCIENTOS ',
-        'NOVECIENTOS '
-    ];
-
-    public static function convertir($number, $moneda = '', $centimos = '', $forzarCentimos = false)
     {
-        $converted = '';
-        $decimales = '';
+        private static $UNIDADES = [
+            '',
+            'UN ',
+            'DOS ',
+            'TRES ',
+            'CUATRO ',
+            'CINCO ',
+            'SEIS ',
+            'SIETE ',
+            'OCHO ',
+            'NUEVE ',
+            'DIEZ ',
+            'ONCE ',
+            'DOCE ',
+            'TRECE ',
+            'CATORCE ',
+            'QUINCE ',
+            'DIECISEIS ',
+            'DIECISIETE ',
+            'DIECIOCHO ',
+            'DIECINUEVE ',
+            'VEINTE '
+        ];
 
-        if (($number < 0) || ($number > 999999999)) {
-            return 'No es posible convertir el numero a letras';
-        }
+        private static $DECENAS = [
+            'VENTI',
+            'TREINTA ',
+            'CUARENTA ',
+            'CINCUENTA ',
+            'SESENTA ',
+            'SETENTA ',
+            'OCHENTA ',
+            'NOVENTA ',
+            'CIEN '
+        ];
 
-        $div_decimales = explode('.',$number);
+        private static $CENTENAS = [
+            'CIENTO ',
+            'DOSCIENTOS ',
+            'TRESCIENTOS ',
+            'CUATROCIENTOS ',
+            'QUINIENTOS ',
+            'SEISCIENTOS ',
+            'SETECIENTOS ',
+            'OCHOCIENTOS ',
+            'NOVECIENTOS '
+        ];
 
-        if(count($div_decimales) > 1){
-            $number = $div_decimales[0];
-            $decNumberStr = (string) $div_decimales[1];
-            if(strlen($decNumberStr) == 2){
-                $decNumberStrFill = str_pad($decNumberStr, 9, '0', STR_PAD_LEFT);
-                $decCientos = substr($decNumberStrFill, 6);
-                $decimales = self::convertGroup($decCientos);
+        public static function convertir($number, $moneda = '', $centimos = '', $forzarCentimos = false)
+        {
+            $converted = '';
+            $decimales = '';
+
+            if (($number < 0) || ($number > 999999999)) {
+                return 'No es posible convertir el numero a letras';
             }
-        }
-        else if (count($div_decimales) == 1 && $forzarCentimos){
-            $decimales = 'CERO ';
-        }
 
-        $numberStr = (string) $number;
-        $numberStrFill = str_pad($numberStr, 9, '0', STR_PAD_LEFT);
-        $millones = substr($numberStrFill, 0, 3);
-        $miles = substr($numberStrFill, 3, 3);
-        $cientos = substr($numberStrFill, 6);
+            $div_decimales = explode('.',$number);
 
-        if (intval($millones) > 0) {
-            if ($millones == '001') {
-                $converted .= 'UN MILLON ';
-            } else if (intval($millones) > 0) {
-                $converted .= sprintf('%sMILLONES ', self::convertGroup($millones));
+            if(count($div_decimales) > 1){
+                $number = $div_decimales[0];
+                $decNumberStr = (string) $div_decimales[1];
+                if(strlen($decNumberStr) == 2){
+                    $decNumberStrFill = str_pad($decNumberStr, 9, '0', STR_PAD_LEFT);
+                    $decCientos = substr($decNumberStrFill, 6);
+                    $decimales = self::convertGroup($decCientos);
+                }
             }
-        }
-
-        if (intval($miles) > 0) {
-            if ($miles == '001') {
-                $converted .= 'MIL ';
-            } else if (intval($miles) > 0) {
-                $converted .= sprintf('%sMIL ', self::convertGroup($miles));
+            else if (count($div_decimales) == 1 && $forzarCentimos){
+                $decimales = 'CERO ';
             }
-        }
 
-        if (intval($cientos) > 0) {
-            if ($cientos == '001') {
-                $converted .= 'UN ';
-            } else if (intval($cientos) > 0) {
-                $converted .= sprintf('%s ', self::convertGroup($cientos));
+            $numberStr = (string) $number;
+            $numberStrFill = str_pad($numberStr, 9, '0', STR_PAD_LEFT);
+            $millones = substr($numberStrFill, 0, 3);
+            $miles = substr($numberStrFill, 3, 3);
+            $cientos = substr($numberStrFill, 6);
+
+            if (intval($millones) > 0) {
+                if ($millones == '001') {
+                    $converted .= 'UN MILLON ';
+                } else if (intval($millones) > 0) {
+                    $converted .= sprintf('%sMILLONES ', self::convertGroup($millones));
+                }
             }
-        }
 
-        if(empty($decimales)){
-            $valor_convertido = $converted . strtoupper($moneda);
-        } else {
-            $valor_convertido = $converted . strtoupper($moneda) . ' CON ' . $decimales . ' ' . strtoupper($centimos);
-        }
+            if (intval($miles) > 0) {
+                if ($miles == '001') {
+                    $converted .= 'MIL ';
+                } else if (intval($miles) > 0) {
+                    $converted .= sprintf('%sMIL ', self::convertGroup($miles));
+                }
+            }
 
-        return $valor_convertido;
-    }
+            if (intval($cientos) > 0) {
+                if ($cientos == '001') {
+                    $converted .= 'UN ';
+                } else if (intval($cientos) > 0) {
+                    $converted .= sprintf('%s ', self::convertGroup($cientos));
+                }
+            }
 
-    private static function convertGroup($n)
-    {
-        $output = '';
-
-        if ($n == '100') {
-            $output = "CIEN ";
-        } else if ($n[0] !== '0') {
-            $output = self::$CENTENAS[$n[0] - 1];
-        }
-
-        $k = intval(substr($n,1));
-
-        if ($k <= 20) {
-            $output .= self::$UNIDADES[$k];
-        } else {
-            if(($k > 30) && ($n[2] !== '0')) {
-                $output .= sprintf('%sY %s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
+            if(empty($decimales)){
+                $valor_convertido = $converted . strtoupper($moneda);
             } else {
-                $output .= sprintf('%s%s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
+                $valor_convertido = $converted . strtoupper($moneda) . ' CON ' . $decimales . ' ' . strtoupper($centimos);
             }
+
+            return $valor_convertido;
         }
 
-        return $output;
-    }
-}
+        private static function convertGroup($n)
+        {
+            $output = '';
 
-    $fecha_formato ='
-        <span>Fecha: </span>
-        <span>'.$today.'</span>
-    ';
+            if ($n == '100') {
+                $output = "CIEN ";
+            } else if ($n[0] !== '0') {
+                $output = self::$CENTENAS[$n[0] - 1];
+            }
+
+            $k = intval(substr($n,1));
+
+            if ($k <= 20) {
+                $output .= self::$UNIDADES[$k];
+            } else {
+                if(($k > 30) && ($n[2] !== '0')) {
+                    $output .= sprintf('%sY %s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
+                } else {
+                    $output .= sprintf('%s%s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
+                }
+            }
+
+            return $output;
+        }
+    }
+
+    $GLOBALS['fecha_formato'] ='<span>'.$today.'</span>';
 
     ob_start();
 
@@ -221,7 +194,8 @@
                 <div style="border-width: 1px; border-style: solid solid solid solid; border-color: black;">
                     <table>
                         <tr>
-                            <td width="70%" align="left">
+                            <td width="2%"></td>
+                            <td width="68%" align="left">
                                 <img width="350" src="../../img/logo_finanzas.png">
                             </td>
                             <td width="30%">
@@ -230,12 +204,10 @@
                                         <td align="center" colspan="3">
                                             <table border=".5">
                                                 <tr>
-                                                    <td colspan="3">Fecha</td>
+                                                    <td>Fecha</td>
                                                 </tr>
                                                 <tr>
-                                                    <td>1</td>
-                                                    <td>2</td>
-                                                    <td>3</td>
+                                                    <td>'.$GLOBALS['fecha_formato'].'</td>
                                                 </tr>
                                             </table>
                                         </td>
@@ -247,44 +219,48 @@
 
                     <table>
                         <tr align="center">
-                            <td colspan="2">ORDEN DE PAGO</td>
-                        </tr>
+                            <td colspan="4">ORDEN DE PAGO</td>
+                        </tr><tr><td></td></tr>
 
                         <tr align="right">
-                            <td colspan="2"><span><b>LICENCIA: </b></span><span>'.$folio.'asdasdasd</span></td>
-                        </tr>
+                            <td width="95%" colspan="3"><span><b>LICENCIA: </b></span><span>'.$GLOBALS['folio'].'</span></td>
+                            <td width="5%"></td>
+                        </tr><tr><td></td></tr>
 
                         <tr>
-                            <td style="width: 20%;"><span><b>Destinatario: </b></span></td>
-                            <td style="width: 80%;"><span>'.$nombre.'asdasdasdasd</span></td>
-                        </tr>
+                            <td width="3%"></td>
+                            <td width="12.5%"><span><b>Destinatario: </b></span></td>
+                            <td class="borderBottom" width="80%" colspan="2"><span>'.$GLOBALS['nombre'].'</span></td>
+                        </tr><tr><td></td></tr>
 
                         <tr>
-                            <td style="width: 100%;" colspan="2"><span>Sírvase liquidar en la Secretaría de Finanzas del H. Ayuntamiento de Jesús María, Ags., la cantidad de:</span>
+                            <td width="3%"></td>
+                            <td style="width: 100%;" colspan="3"><span>Sírvase liquidar en la Secretaría de Finanzas del H. Ayuntamiento de Jesús María, Ags., la cantidad de:</span>
                             </td>
-                        </tr>
+                        </tr><tr><td></td></tr>
 
                         <tr>
-                            <td style="width: 20%;">
-                                <span>$'.$monto.'500</span>
+                            <td width="3%"></td>
+                            <td width="20%" align="center" class="borderBottom"><span>$'.$GLOBALS['monto'].'</span></td>
+                            <td width="1%">,</td>
+                            <td width= "71.5%" class="borderBottom">
+                                <span>('.$GLOBALS['letras'].').</span>
                             </td>
-
-                            <td style="width: 80%;">
-                                <span>('.$letras.'.)quinientos pesos</span>
-                            </td>
-                        </tr>
+                        </tr><tr><td></td></tr>
 
                         <tr>
-                            <td style="width: 45%;"><b>Por concepto de trámite de:</b> </td>
-                            <td style="width: 55%;">Expedición de licencia para negocio de <b>'.$giro.'</b>.</td>
-                        </tr>
+                            <td width="3%"></td>
+                            <td width="25.5%"><b>Por concepto de trámite de:</b> </td>
+                            <td width="67%" class="borderBottom" colspan="2">Expedición de licencia para negocio de <b>'.$GLOBALS['giro'].'.</b></td>
+                        </tr><tr><td></td></tr><tr><td></td></tr>
+
                         <tr align="right">
-                            <td><b>ATENTAMENTE: </b>'.$_SESSION['nombre_completo'].'.</td>
+                            <td colspan="4"><b>ATENTAMENTE: </b>'.$_SESSION['nombre_completo'].'.</td>
                         </tr>
                     </table>
-                </div>    ';
+                </div>';
     
-            $this->writeHTMLCell('', '', '', 10, $cabeza, $border=0, $ln=2, $fill=0, $reseth=true, $align='L', $autopadding=true);
+            $this->writeHTML($cabeza, false, 0, false, false, '');
         }  
     }
 
@@ -309,6 +285,6 @@
         mkdir('../../assets/expedientes/'.$folio.'/docs/pagos/');
     }
 
-    $pdf->Output($_SERVER['DOCUMENT_ROOT'] . 'assets/expedientes/'.$folio.'/docs/pagos/Orden de Pago.pdf', 'FI');
+    $pdf->Output($_SERVER['DOCUMENT_ROOT'] . 'assets/expedientes/'.$folio.'/docs/pagos/Orden de Pago de Licencia.pdf', 'FI');
     
 ?>
