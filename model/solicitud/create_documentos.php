9832ff3dfd4e247fa4e75af4095816f6091d38c0
Jose Manuel <31418107+hosce@users.noreply.github.com>
2021-08-09 16:19:13 -0500

    Update create_documentos.php
    Crea carpeta y sube comprobante de pago

--- model/solicitud/create_documentos.php
+++ model/solicitud/create_documentos.php
@@ -1,7 +1,7 @@
 <?php
 
 	include('../../controller/solicitud/funciones_solicitud.php');
-
+	
 	//VARIABLE DEL CHECK PARA SOLICITAR NÚMERO OFICIAL A SEDATUM
 	//$check = $_POST['checkNumOficial'];//Manda la variable solo si está checada, de lo contrario no manda nada.
 	//POSICIÓN DEL POST PARA EL COMPROBANTE --> $_POST['cp_no'];
@@ -10,11 +10,23 @@
 	$id_persona = $_POST['id_per'];
 	$id_dg = $_POST['id_dg'];
 	$id_dimensiones = $_POST['id_dim'];
-	$ruta = "../../assets/expedientes/".$docs."/docs/documentacion/";
+	$ruta = "../../assets/expedientes/".$docs."/docs/documentacion";
 	$titulo = $ruta.'/'.basename($_FILES['titulo']['name']);
 	$predia = $ruta.'/'.basename($_FILES['pred']['name']);
 	$ine = $ruta.'/'.basename($_FILES['ine']['name']);
 	$contrato = $ruta.'/'.basename($_FILES['contrato']['name']);
+	if(isset($_FILES['cp_no']))
+	{
+		$ruta_pago = "../../assets/expedientes/".$docs."/docs/pagos";
+		if(!is_dir($ruta_pago))
+		{
+			if(!mkdir($ruta_pago,0777, true))
+			{
+				echo "Fallo la creacion de carpeta";
+			}		
+		}
+		$cp_no = $ruta_pago.'/'.basename($_FILES['cp_no']['name']);
+	}
 	$noficial = $ruta.'/'.basename($_FILES['no']['name']);
 	$noficial = $id_expediente = 0; 
 	if(isset($_POST['checkNumOficial']))
@@ -48,6 +60,7 @@
 			move_uploaded_file($_FILES['ine']['tmp_name'], $ine);
 			move_uploaded_file($_FILES['contrato']['tmp_name'], $contrato);
 			move_uploaded_file($_FILES['no']['tmp_name'], $noficial);
+			move_uploaded_file($_FILES['cp_no']['tmp_name'], $cp_no);
 			if($tipo_persona == 1)
 			{
 				move_uploaded_file($_FILES['acta']['tmp_name'], $acta);
