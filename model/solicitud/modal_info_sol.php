7e405de5b42af640f223b2df22787b2158d748bd
fernandojm94 <emmanuel.martinez@jesusmaria.gob.mx>
2021-09-09 13:26:07 -0500

    Quita botones de aprobar y rechazar
    Quita los botones de aprobar y rechazar si ya finalizó la solicitud

--- model/solicitud/modal_info_sol.php
+++ model/solicitud/modal_info_sol.php
@@ -12,7 +12,7 @@
 	$rechaz_btn = '<button type="button" class="btn btn-danger" onclick="actualiza_status('.$id.',2,0,0);"><i class="fa fa-ban"></i>&nbsp;Rechazar Solicitud</button>';
 	$aprob_btn = '<button type="button" class="btn btn-success" onclick="actualiza_status('.$id.',1,0,0);"><i class="fa fa-check"></i>&nbsp;Aprobar Solicitud</button>';	
 
-	if (($pantalla == 1 && $etapa == 2) || ($pantalla == 1 && $etapa == 3) || ($pantalla == 2 && $etapa == 3) || ($pantalla == 1 && $etapa == 4) || ($pantalla == 1 && $etapa == 5) || ($pantalla == 1 && $etapa == 6)) {
+	if (($pantalla == 1 && $etapa == 2) || ($pantalla == 1 && $etapa == 3) || ($pantalla == 2 && $etapa == 3) || ($pantalla == 1 && $etapa == 4) || ($pantalla == 1 && $etapa == 5) || ($pantalla == 1 && $etapa == 6) || $etapa == 7 ) {
 		
 		$rechaz_btn = '';
 		$aprob_btn = '';
