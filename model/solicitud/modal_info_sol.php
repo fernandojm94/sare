871f0640420f20e0dbec9894c9ed757dd27c2308
fernandojm94 <emmanuel.martinez@jesusmaria.gob.mx>
2021-10-25 15:13:47 -0500

    Cambia el input por textarea, en el campo de servicios existentes.

--- model/solicitud/modal_info_sol.php
+++ model/solicitud/modal_info_sol.php
@@ -439,16 +439,22 @@
 													<i class="fa fa-map-marker green bigger-110"></i>&nbsp;
 														<input id="de12" name="de12" type="text" class="sinborde"  value="<?=$establecimiento['manzana'];?>" disabled size="50">
 													</div>
-												</div>
+												</div>														
 
 												<div class="profile-info-row">
 													<div class="profile-info-name">Servicios existentes: </div>
 
 													<div class="profile-info-value">
-													<i class="fa fa-cogs green bigger-110"></i>&nbsp;
-														<label class="input-sizer">
-															<input id="de13" name="de13" type="text" class="sinborde"  value="<?=$establecimiento['servicios_existentes'];?>" disabled>
-														</label>	
+														<div style="display: flex;">
+															<div style="align-self: center;">
+																<i class="fa fa-cogs green bigger-110"></i>&nbsp;
+															</div>
+
+															<div>
+																<label class="input-sizer">
+																	<textarea rows="1" id="de13" name="de13" type="text" class="sinborde"  value="<?=$establecimiento['servicios_existentes'];?>" disabled><?=$establecimiento['servicios_existentes'];?></textarea>
+																</label>
+															</div>
 													</div>
 												</div>
 											</form>
