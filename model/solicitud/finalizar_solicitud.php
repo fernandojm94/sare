bb88e0dc91beb80bf1518add65131523e8e9fe03
Jose Manuel <31418107+hosce@users.noreply.github.com>
2022-08-08 15:57:55 -0500

    Update finalizar_solicitud.php
    Crea expediente y ventanilla

--- model/solicitud/finalizar_solicitud.php
+++ model/solicitud/finalizar_solicitud.php
@@ -1,3 +1,35 @@
 <?php
-	var_dump($_POST);
-?>
+var_dump($_POST);
+$data = explode(",", $_POST['img_map']);	
+$data_64 = base64_decode($data[1]);	
+$data = getImageSizeFromString($data_64);
+$ruta_img = "../../assets/expedientes/".$docs."/docs/mapa.".substr($data['mime'], 6);
+
+$docs =str_replace(' ', '-', str_replace(':', '-',str_replace('/', '-', $_POST['folio'])));	
+$id_persona = $_POST['id_per'];
+$id_dg = $_POST['id_dg'];
+$id_dimensiones = $_POST['id_dim'];
+$noficial = $tipo_persona = 0;
+if ($_POST['tipo_persona'] == 'p_moral') 
+{
+		$tipo_persona = 1;
+}
+
+if(isset($_POST['checkNumOficial']))
+{
+	$noficial = 1;	
+}
+
+$id_expediente = create_expediente($docs, $tipo_persona, $id_persona, $id_dg, $id_dimensiones, $noficial);
+if($id_expediente)
+{
+	if(create_ventanilla_id($id_expediente))
+	{
+	$mensaje = "correcto";
+	}else{
+		$mensaje = "error";
+	}
+}else{
+	$mensaje = "error";
+}
+echo $mensaje;
