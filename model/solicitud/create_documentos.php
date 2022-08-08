0b3df9989476a4d1f0aed5e800a3e2f2e8e6ae0c
Jose Manuel <31418107+hosce@users.noreply.github.com>
2022-08-08 13:18:54 -0500

    Update create_documentos.php
    Modifica forma de subir archivos

--- model/solicitud/create_documentos.php
+++ model/solicitud/create_documentos.php
@@ -1,31 +1,42 @@
 <?php
-
-	var_dump($_FILES); 
-	var_dump($_POST); 
-		exit();
-
-	include('../../controller/solicitud/funciones_solicitud.php');
 	
-	//VARIABLE DEL CHECK PARA SOLICITAR NÚMERO OFICIAL A SEDATUM
-	//$check = $_POST['checkNumOficial'];//Manda la variable solo si está checada, de lo contrario no manda nada.
-	//POSICIÓN DEL POST PARA EL COMPROBANTE --> $_POST['cp_no'];
+		$folio = "SARE/".date("Y/m/d H:m:s");	
 	
-	$folio = "SARE/".date("Y/m/d H:m:s");
+	
 	$docs =str_replace(' ', '-', str_replace(':', '-',str_replace('/', '-', $folio)));	
-	$data = explode(",", $_POST['img_map']);	
-	$data_64 = base64_decode($data[1]);	
-	$data = getImageSizeFromString($data_64);
-	$ruta_img = "../../assets/expedientes/".$docs."/docs/mapa.".substr($data['mime'], 6);	
-	
-
-	$id_persona = $_POST['id_per'];
-	$id_dg = $_POST['id_dg'];
-	$id_dimensiones = $_POST['id_dim'];
 	$ruta = "../../assets/expedientes/".$docs."/docs/documentacion";
-	$titulo = $ruta.'/'.basename($_FILES['titulo']['name']);
-	$predia = $ruta.'/'.basename($_FILES['pred']['name']);
-	$ine = $ruta.'/'.basename($_FILES['ine']['name']);
-	$contrato = $ruta.'/'.basename($_FILES['contrato']['name']);
+	if(!is_dir($ruta))
+	{
+		if(!mkdir($ruta,0777, true))
+		{
+			echo "Fallo la creacion de carpeta";
+		}		
+	}
+	if(isset($_FILES['titulo']))
+	{
+		$archivo = $ruta.'/'.basename($_FILES['titulo']['name']);
+		$origi = $_FILES['titulo']['tmp_name'];		
+	}
+	if(isset($_FILES['pred']))
+	{
+		$archivo = $ruta.'/'.basename($_FILES['pred']['name']);
+		$origi = $_FILES['pred']['tmp_name'];		
+	}
+	if(isset($_FILES['ine']))
+	{
+		$archivo = $ruta.'/'.basename($_FILES['ine']['name']);
+		$origi = $_FILES['ine']['tmp_name'];
+	}
+	if(isset($_FILES['contrato']))
+	{
+		$archivo = $ruta.'/'.basename($_FILES['contrato']['name']);
+		$origi = $_FILES['contrato']['tmp_name'];
+	}
+	if(isset($_FILES['no']))
+	{
+		$archivo = $ruta.'/'.basename($_FILES['no']['name']);
+		$origi = $_FILES['no']['tmp_name'];
+	}
 	if(isset($_FILES['cp_no']))
 	{
 		$ruta_pago = "../../assets/expedientes/".$docs."/docs/pagos";
@@ -36,63 +47,29 @@
 				echo "Fallo la creacion de carpeta";
 			}		
 		}
-		$cp_no = $ruta_pago.'/'.basename($_FILES['cp_no']['name']);
+		$archivo = $ruta_pago.'/'.basename($_FILES['cp_no']['name']);
+		$origi = $_FILES['cp_no']['tmp_name'];
 	}
-	$noficial = $ruta.'/'.basename($_FILES['no']['name']);
-	$noficial = $id_expediente = 0; 
-	if(isset($_POST['checkNumOficial']))
+	if(isset($_FILES['acta']))
 	{
-		$noficial = 1;	
+		$archivo = $ruta.'/'.basename($_FILES['acta']['name']);
+		$origi = $_FILES['acta']['tmp_name'];
 	}
-
-	$tipo_persona = $_POST['tipo_persona'];
-
-	if ($tipo_persona == 'p_moral') {
-		$tipo_persona = 1;
-	}else{
-		$tipo_persona = 0;
-	}
-
-	if($tipo_persona == 1)
+	if(isset($_FILES['poder']))
 	{
-		$acta = $ruta.'/'.basename($_FILES['acta']['name']);
-		$poder = $ruta.'/'.basename($_FILES['poder']['name']);
-		$solicitud = $ruta.'/'.basename($_FILES['solicitud']['name']);
+		$archivo = $ruta.'/'.basename($_FILES['poder']['name']);
+		$origi = $_FILES['poder']['tmp_name'];
 	}
-
-	if(!is_dir($ruta))
+	if(isset($_FILES['solicitud']))
 	{
-		if(!mkdir($ruta,0777, true))
+		$archivo = $ruta.'/'.basename($_FILES['solicitud']['name']);
+		$origi = $_FILES['solicitud']['tmp_name'];
+	}
+	if(move_uploaded_file($origi, $archivo))
 		{
-			echo "Fallo la creacion de carpeta";
-		}else{			
-			move_uploaded_file($_FILES['titulo']['tmp_name'], $titulo);
-			move_uploaded_file($_FILES['pred']['tmp_name'], $predia);
-			move_uploaded_file($_FILES['ine']['tmp_name'], $ine);
-			move_uploaded_file($_FILES['contrato']['tmp_name'], $contrato);
-			move_uploaded_file($_FILES['no']['tmp_name'], $noficial);
-			move_uploaded_file($_FILES['cp_no']['tmp_name'], $cp_no);
-			file_put_contents($ruta_img, $data_64);
-
-			if($tipo_persona == 1)
-			{
-				move_uploaded_file($_FILES['acta']['tmp_name'], $acta);
-				move_uploaded_file($_FILES['poder']['tmp_name'], $poder);
-				move_uploaded_file($_FILES['solicitud']['tmp_name'], $solicitud);
-			}
-		}
-	}
-	$id_expediente = create_expediente($docs, $tipo_persona, $id_persona, $id_dg, $id_dimensiones, $noficial);
-	if($id_expediente)
-	{
-		if(create_ventanilla_id($id_expediente))
-		{
-		$mensaje = "correcto";
+			$mensaje = "correcto";
 		}else{
 			$mensaje = "error";
 		}
-	}else{
-		$mensaje = "error";
-	}
 
-	echo $mensaje;
+echo $mensaje.",".$folio;
